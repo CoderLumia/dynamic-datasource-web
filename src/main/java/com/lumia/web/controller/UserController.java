72b515fb998700a805941cfa1dc9cce7a462247c
zenglin <zenglin@snqu.com>
2020-07-14 10:55:43 +0800

    dynamic dataSource

--- src/main/java/com/lumia/web/controller/UserController.java
+++ src/main/java/com/lumia/web/controller/UserController.java
@@ -6,6 +6,7 @@ import com.lumia.web.entity.User;
 import com.lumia.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,14 +38,27 @@ public class UserController {
         return result;
     }
 
-    @GetMapping("/findAllByDataSource")
-    public Map<String , List<User>> findAllByDataSource() {
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setUrl("jdbc:mysql://node2:3306/sndo_copy?useUnicode=true&characterEncoding=utf-8&useSSL=false");
-        druidDataSource.setUsername("root");
-        druidDataSource.setPassword("root");
-        DynamicDataSource.dataSourcesMap.put("sndo_copy", druidDataSource);
-        DynamicDataSource.setDataSource("sndo_copy");
+    @GetMapping("/findAllByDataSource/{service}")
+    public Map<String , List<User>> findAllByDataSource(@PathVariable("service") String service) {
+
+        String url = "jdbc:mysql://192.168.0.215:3306/%s?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+        if ("sndo".equals(service)) {
+            DynamicDataSource.setDataSource("defaultDataSource");
+        } else {
+            boolean containsKey = DynamicDataSource.dataSourcesMap.containsKey(service);
+            if (containsKey) {
+                DynamicDataSource.setDataSource(service);
+            } else {
+                String format = String.format(url, service);
+                DruidDataSource druidDataSource = new DruidDataSource();
+                druidDataSource.setUrl(format);
+                druidDataSource.setUsername("root");
+                druidDataSource.setPassword("root");
+                DynamicDataSource.dataSourcesMap.put(service, druidDataSource);
+                DynamicDataSource.setDataSource(service);
+            }
+        }
+
         Map<String, List<User>> result = new HashMap<>();
         List<User> all = userService.findAll();
         result.put("data", all);
